d0aed1de9f5b775ecd800ed6d764fa6c52983eff
larriag13 <luis.arriagada13@inacapmail.cl>
2026-09-21 11:27:22 -0300

    main avanzado

--- main.py
+++ main.py
@@ -1,11 +1,63 @@
 from paciente import Paciente
+pacientes: list[Paciente] = []
+
+def agregar_paciente()->None:
+    rut= input("Ingrese el RUT del paciente: ")
+    nombre = input("Ingrese el nombre del paciente: ")
+    edad = int(input("Ingrese la edad del paciente: "))
+    print("Previsiones disponibles: ")
+    print("1.- Fonasa")
+    print("2.- Isapre")
+    prevision = input("Seleccione la previsión del paciente: ")
+    if prevision == "1":
+        prevision = "Fonasa"
+    else:
+        prevision = "Isapre"
+
+    paciente = Paciente(rut, nombre, edad, prevision)
+    pacientes.append(paciente)
+    print("Paciente agregado exitosamente.")
+
+def leer_numero(mensaje:str)->int:
+    while True:
+        try:
+            numero = int(input(mensaje))
+            return numero
+        except ValueError:
+            print("Por favor, ingrese un número válido.")
+
+def menu()->int:
+    opcion=-1
+    while opcion<0 or opcion>5:
+        print("Menú de clinica")
+        print("1.- Agregar paciente")
+        print("2.- Editar paciente")
+        print("3.- Eliminar paciente")
+        print("4.- Imprimir un paciente")
+        print("5.- Imprimir todos los pacientes")
+        print("0-. Salir")
+        opcion = leer_numero("Seleccione una opción: ")
+    return opcion
 
 def main():
-    # Crear paciente con el constructor __init__
-    p1 = Paciente("11.111.111-1", "Luis Arriagada", 40, "Isapre")
-    # Mostrar información del paciente
-    # __str__ es llamado automáticamente al imprimir el objeto
-    print(p1)
-
+    op=-1
+    while op!=0:
+        op=menu()
+        if op==1:
+            print("Agregando paciente")
+        elif op==2:
+            print("Editando paciente")
+        elif op==3:
+            print("Eliminando paciente")
+        elif op==4:
+            print("Imprimiendo un paciente")
+        elif op==5:
+            print("Imprimiendo todos los pacientes")
+        elif op==0:
+            print("Saliendo del programa")
+    
+    
 if __name__ == "__main__":
     main()  
+
+#https://github.com/larriag13/01_ejemplo_poos_n2p1c2.git 
