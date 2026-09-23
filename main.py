9d32b7edcae5dde73c733fa3bcfe4fa7ea3e94d1
larriag13 <luis.arriagada13@inacapmail.cl>
2026-09-23 09:48:11 -0300

    ejemplo paciente ok

--- main.py
+++ main.py
@@ -39,20 +39,84 @@ def menu()->int:
         opcion = leer_numero("Seleccione una opción: ")
     return opcion
 
+def buscar_paciente()->Paciente:
+    rut = input("Ingrese el R.U.T. del paciente a buscar: ")
+    for paciente in pacientes:
+        if paciente.rut == rut:
+            return paciente
+    return None
+
+def imprimir_paciente()->None:
+    paciente=buscar_paciente()
+    if paciente:
+        print(paciente)
+    else:
+        print("Paciente no encontrado.")
+
+def imprimir_pacientes()->None:
+    if pacientes:
+        for paciente in pacientes:
+            print(paciente)
+    else:
+        print("No hay pacientes registrados.")
+
+def editar_paciente()->None:
+    paciente=buscar_paciente()
+    if paciente:
+        print(paciente)
+        print("Menu de edición de paciente")
+        print("1.- Editar nombre")
+        print("2.- Editar edad")
+        print("3.- Editar previsión")
+        opcion = leer_numero("Seleccione una opción: ")
+        if opcion == 1:
+            nuevo_nombre = input("Ingrese el nuevo nombre: ")
+            paciente.nombre = nuevo_nombre
+        elif opcion == 2:
+            nueva_edad = leer_numero("Ingrese la nueva edad: ")
+            paciente.edad = nueva_edad
+        elif opcion == 3:
+            print("Previsiones disponibles: ")
+            print("1.- Fonasa")
+            print("2.- Isapre")
+            print("0.- No hacer cambios")
+            nueva_prevision = input("Seleccione la nueva previsión: ")
+            if nueva_prevision == "1":
+                paciente.prevision = "Fonasa"
+            elif nueva_prevision == "2":
+                paciente.prevision = "Isapre"
+            else:
+                print("No se realizaron cambios en la previsión.")
+    else:
+        print("Paciente no encontrado.")
+
+def eliminar_paciente()->None:
+    paciente=buscar_paciente()
+    if paciente:
+        pacientes.remove(paciente)
+        print("Paciente eliminado exitosamente.")
+    else:
+        print("Paciente no encontrado.")
+
 def main():
     op=-1
     while op!=0:
         op=menu()
         if op==1:
             print("Agregando paciente")
+            agregar_paciente()
         elif op==2:
             print("Editando paciente")
+            editar_paciente()
         elif op==3:
             print("Eliminando paciente")
+            eliminar_paciente()
         elif op==4:
             print("Imprimiendo un paciente")
+            imprimir_paciente()
         elif op==5:
             print("Imprimiendo todos los pacientes")
+            imprimir_pacientes()
         elif op==0:
             print("Saliendo del programa")
     
